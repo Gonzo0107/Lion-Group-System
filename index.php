8585ba45ae41ca615466de9a14bd5e7536d30e38
Gonzo0107 <83229090+Gonzo0107@users.noreply.github.com>
2021-06-28 07:27:29 -0500

    Versión 1.0
    Primera versión con el código de la pagina principal

---
+++ index.php
@@ -0,0 +1,287 @@
+<?php include_once 'includes/templates/header.php';?>
+
+  <section class="seccion contenedor">
+    <h2 class="lines-effect">Expertos en seguridad social</h2>
+    <p>
+      Realizamos procesos de traslados a todas las EPS y fondos de pensiones, asesoramos empresas, acompañando sus
+      procesos de contratación, liquidamos planillas. ¿Eres independiente y no sabes como realizar los procesos de
+      afiliación a las entidades de seguridad social?, nosotros te ayudamos.
+    </p>
+  </section>
+  <!--seccion-->
+  <section class="programa">
+    <div class="contenedor-video">
+      <video autoplay loop poster="bg-talleres.jpg" muted>
+        <source src="video/video.mp4" type="video/mp4">
+        <source src="video/video.webm" type="video/webm">
+        <source src="video/video.ogv" type="video/ogv">
+      </video>
+    </div>
+    <!--.contenedor-video-->
+    <div class="contenido-servicios">
+      <div class="contenedor">
+        <div class="programa-evento">
+          <h2>NUESTROS SERVICIOS</h2>
+          <nav class="menu-servicios">
+            <a href="#traslados"><i class="fas fa-heartbeat"></i>TRASLADOS EPS</a>
+            <a href="#pensiones"><i class="fas fa-money-check-alt"></i>PENSIONES</a>
+            <a href="#independientes"><i class="fas fa-user-md"></i>INDEPENDIENTES</a>
+
+          </nav>
+
+          <div id="traslados" class="info-tdt ocultar clearfix">
+            <div class="detalle-servicio">
+              <h3>Condiciones para traslado entre EPS</h3>
+              <p><i class="fas fa-hourglass-half"></i>Tener mínimo un año de afiliado en tu actual EPS</p>
+              <p><i class="fas fa-money-bill-alt"></i>No tener niguna deuda en tu actual EPS cómo independiente</p>
+              <p><i class="fas fa-user-md"></i>Que no te encuentres en un tratamiento médico de alta complejidad</p>
+              <p><i class="fas fa-people-arrows"></i>Trasladar todo tu grupo familiar completo, tal cual como se
+                encuentra en tu EPS</p>
+
+            </div>
+            <div class="detalle-servicio">
+              <h3>Documentos requeridos</h3>
+              <p><i class="fas fa-id-card"></i>Documento escaneado del cotizante</p>
+              <p><i class="fas fa-baby"></i> Copia de registro civil (RC) para hijos menores de 7 años, Copia
+                de RC + Documento de identidad para hijos mayores de 7 años, hasta los 25 años (si aplica) </p>
+              <p><i class="fab fa-gratipay"></i>Copia del documento escaneado de esposo o pareja (si aplica)</p>
+              <p><i class="fas fa-file-pdf"></i>Copia del RC del cotizante y copia del documento de los padres (si
+                aplica)</p>
+            </div>
+            <a href="#" class="boton float-rigth">VER TODO</a>
+          </div>
+          <!--servicios-->
+
+          <div id="pensiones" class="info-tdt ocultar clearfix">
+            <div class="detalle-servicio">
+              <h3>Iniciales y traslados de AFP</h3>
+              <p><i class="fas fa-hourglass-half"></i>Tener mínimo un año de afiliado en tu actual EPS</p>
+              <p><i class="fas fa-money-bill-alt"></i>No tener niguna deuda en tu actual EPS cómo independiente</p>
+              <p><i class="fas fa-user-md"></i>Que no te encuentres en un tratamiento médico de alta complejidad</p>
+              <p><i class="fas fa-people-arrows"></i>Trasladar todo tu grupo familiar completo, tal cual como se
+                encuentra en tu EPS</p>
+
+            </div>
+            <div class="detalle-servicio">
+              <h3>Documentos requeridos</h3>
+              <p><i class="fas fa-id-card"></i>Documento escaneado del cotizante</p>
+              <p><i class="fas fa-baby"></i> Copia de registro civil (RC) para hijos menores de 7 años, Copia
+                de RC + Documento de identidad para hijos mayores de 7 años, hasta los 25 años (si aplica) </p>
+              <p><i class="fab fa-gratipay"></i>Copia del documento escaneado de esposo o pareja (si aplica)</p>
+              <p><i class="fas fa-file-pdf"></i>Copia del RC del cotizante y copia del documento de los padres (si
+                aplica)</p>
+            </div>
+            <a href="#" class="boton float-rigth">VER TODO</a>
+          </div>
+          <!--servicios-->
+
+          <div id="independientes" class="info-tdt ocultar clearfix">
+            <div class="detalle-servicio">
+              <h3>Afiliaciones para independiente</h3>
+              <p><i class="fas fa-hourglass-half"></i>Tener mínimo un año de afiliado en tu actual EPS</p>
+              <p><i class="fas fa-money-bill-alt"></i>No tener niguna deuda en tu actual EPS cómo independiente</p>
+              <p><i class="fas fa-user-md"></i>Que no te encuentres en un tratamiento médico de alta complejidad</p>
+              <p><i class="fas fa-people-arrows"></i>Trasladar todo tu grupo familiar completo, tal cual como se
+                encuentra en tu EPS</p>
+
+            </div>
+            <div class="detalle-servicio">
+              <h3>Documentos requeridos</h3>
+              <p><i class="fas fa-id-card"></i>Documento escaneado del cotizante</p>
+              <p><i class="fas fa-baby"></i> Copia de registro civil (RC) para hijos menores de 7 años, Copia
+                de RC + Documento de identidad para hijos mayores de 7 años, hasta los 25 años (si aplica) </p>
+              <p><i class="fab fa-gratipay"></i>Copia del documento escaneado de esposo o pareja (si aplica)</p>
+              <p><i class="fas fa-file-pdf"></i>Copia del RC del cotizante y copia del documento de los padres (si
+                aplica)</p>
+            </div>
+            <a href="#" class="boton float-rigth">VER TODO</a>
+          </div>
+          <!--servicios-->
+        </div>
+        <!--programa-evento-->
+      </div>
+      <!--contenedor-->
+    </div>
+    <!--contenido-programa-->
+  </section>
+  <!--programa-->
+
+  <section class="aliados contenedor seccion">
+    <h2>nuestros aliados</h2>
+    <ul class="lista-aliados clearfix">
+      <li>
+        <div class="aliado">
+          <img src="img/salud1.jpg" alt="logo salud total">
+          <p>SALUD TOTAL</p>
+        </div>
+      </li>
+      <li>
+        <div class="aliado">
+          <img src="img/salud2.jpg" alt="logo sanitas">
+          <p>SANITAS EPS</p>
+        </div>
+      </li>
+      <li>
+        <div class="aliado">
+          <img src="img/salud3.jpg" alt="logo famisanar">
+          <p>FAMISANAR EPS</p>
+        </div>
+      </li>
+      <li>
+        <div class="aliado">
+          <img src="img/salud4.jpg" alt="logo nueva eps">
+          <p>NUEVA EPS</p>
+        </div>
+      </li>
+      <li>
+        <div class="aliado">
+          <img src="img/pension.jpg" alt="logo porvenir">
+          <p>PORVENIR</p>
+        </div>
+      </li>
+      <li>
+        <div class="aliado">
+          <img src="img/pension2.jpg" alt="logo proteccion">
+          <p>PROTECCION</p>
+        </div>
+      </li>
+    </ul>
+  </section>
+  <div class="contador parallax">
+    <div class="contenedor">
+      <ul class="resumen-cobertura clearfix">
+        <li>
+          <p class="numero"></p>EPS
+        </li>
+        <li>
+          <p class="numero"></p>CAJAS DE COMPENSACIÓN
+        </li>
+        <li>
+          <p class="numero"></p>FONDOS DE PENSIÓN
+        </li>
+        <li>
+          <p class="numero"></p>CIUDADES
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  
+
+  <section class="precios seccion">
+    <h2>INFORMACIÓN servicios</h2>
+    <div class="contenedor">
+      <ul class="lista-precios clearfix">
+        <li>
+          <div class="tabla-precio">
+            <h3>Traslados EPS</h3>
+            <p class="numero">$0</p>
+            <ul>
+              <li>Asesoria sobre traslado de EPS</li>
+              <li>Proceso y seguimiento del traslado</li>
+              <li>Asesoría pos-traslado</li>
+            </ul>
+            <a href="#" class="boton hollow">mas info</a>
+          </div>
+        </li>
+        <li>
+          <div class="tabla-precio">
+            <h3>FONDOS DE PENSIONES</h3>
+            <p class="numero">$0</p>
+            <ul>
+              <li>Asesoria sobre afiliación a AFP</li>
+              <li>Proceso y seguimiento del afiliación</li>
+              <li>Asesoría pos-afiliación</li>
+            </ul>
+            <a href="#" class="boton">mas info</a>
+          </div>
+        </li>
+
+        <li>
+          <div class="tabla-precio">
+            <h3>INDEPENDIENTES</h3>
+            <p class="numero">$0</p>
+            <ul>
+              <li>Asesoria de procesos de afiliación</li>
+              <li>Procesos de liquidación de aportes</li>
+              <li>Afiliación a arl-eps-afp-ccf</li>
+            </ul>
+            <a href="#" class="boton hollow">mas info</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <div id="mapa" class="mapa"></div>
+  <section class="seccion">
+    <h2>TESTIMONIALES</h2>
+    <div class="testimoniales contenedor clearfix">
+      <div class="testimonial">
+        <blockquote>
+          <p>La gestión de las contrataciones en nuestra compañia tiene el privilegio de contar con el apoyo de
+            Grupo Gestión, un grupo de profesionales expertos en lo que hacen. </p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimonal1.jpg" alt="imagen testimonial">
+            <cite>Gabriela Sabogal <span>Gerente Recursos Humanos Contratamos LTDA </span></cite>
+          </footer>
+        </blockquote>
+      </div><!-- testimonial -->
+
+      <div class="testimonial">
+        <blockquote>
+          <p>Excelente servicio, mi traslado a EPS Sanitas fue aprobado el mismo día, muy profesional todo el personal y
+            la información es muy clara, los recomiendo.</p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimoniales2.jpg" alt="imagen testimonial">
+            <cite>Angélica Gardeazabal <span>Ejecutiva Comercial Atento SA </span></cite>
+          </footer>
+        </blockquote>
+      </div><!-- testimonial -->
+
+      <div class="testimonial">
+        <blockquote>
+          <p>Mi traslado de Fondo de pensiones contó con una asesoría muy completa, siempre me hicieron consciente de
+            los riesgos, su honestidad me llenó de mucha confianza.</p>
+          <footer class="info-testimonial clearfix">
+            <img src="img/testimonial3.jpg" alt="imagen testimonial">
+            <cite>Felipe Castro Tamayo <span>Conductor de MacPollo </span></cite>
+          </footer>
+        </blockquote>
+      </div><!-- testimonial -->
+    </div>
+  </section>
+
+  <div class="newsletter parallax">
+    <div class="contenido contenedor">
+      <p>Regístrate al Newsletter: </p>
+      <h3>Grupo Gestión <span>L&C</span></h3>
+      <a href="#" class="boton transparente">Registro</a>
+    </div>
+    <!--.contenido---->
+  </div>
+  <!--.newsletter-->
+
+  <section class="seccion">
+    <h2>¡Solo queda esto para que termine el año, asesorate ya!</h2>
+    <div class="reloj contenedor">
+      <ul class="clearfix">
+        <li>
+          <p id="dias" class="numero"></p>días
+        </li>
+        <li>
+          <p id="horas" class="numero"></p>horas
+        </li>
+        <li>
+          <p id="minutos" class="numero"></p>minutos
+        </li>
+        <li>
+          <p id="segundos" class="numero"></p>segundos
+        </li>
+
+      </ul>
+    </div>
+  </section>
+
+  <?php include_once 'includes/templates/footer.php';?>
+
