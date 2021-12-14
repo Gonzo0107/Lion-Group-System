cbd62d26b30cc7c57dddd0412bf2778abbb11d3e
Gonzo0107 <83229090+Gonzo0107@users.noreply.github.com>
2021-12-14 13:56:12 -0500

    Pagina principal de Grupo Gestion L Y C S.A.S
    Pagina principal finalizada de Grupo Gestion L YC S.A.S

---
+++ Lion Group/includes/templates/header.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Grupo Gestión L&C</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Oswald:wght@200;300;400;500&family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+  <header class="site-header">
+
+    <div class="hero">
+
+      <div class="contenido-header">
+
+        <nav class="redes-sociales">
+          <a href="https://www.facebook.com/Grupo-Gesti%C3%B3n-LC-103232958078231"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.instagram.com/grupogestionlyc/?hl=es-la"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.youtube.com/channel/UCnF8Uya7kl3BSWoryWA8D5A"><i class="fab fa-youtube"></i></a>
+        </nav>
+        <div class="informacion-evento">
+          <div class="clearfix">
+            <p class="fecha"><i class="fas fa-calendar-alt"></i>15/06/2021</p>
+            <p class="ciudad"><i class="fas fa-map-marked-alt"></i>Bogotá, COL</p>
+          </div>
+          <h1 class="nombre-sitio">GRUPO GESTIÓN <span>L&C</span></h1>
+          <p class="slogan">asesoría y consultoría en <span>seguridad social</span></p>
+
+        </div>
+        <!--informacion-evento-->
+
+      </div>
+
+
+    </div>
+    <!--hero-->
+
+  </header>
+  <div class="barra">
+    <div class="contenedor clearfix">
+      <div class="logo">
+        <img src="img/logo.svg" alt="#" a href="">
+      </div>
+      <div class="menu-movil">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="navegacion-principal clearfix">
+        <a href="#">BLOG</a>
+        <a href="http://localhost/lion_group_system/lion group/nosotros.html">NOSOTROS</a>
+        <a href="#">CONTACTENOS</a>
+        <a href="http://localhost/lion_group_system/login/login.php">LION GROUP SYSTEM</a>
+      </nav>
+    </div>
+    <!--.Contenedor-->
+  </div>
+  <!--.Barra-->
